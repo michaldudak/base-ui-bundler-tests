8469668c52f5e5879625c364f18fbb5555934a73
Michał Dudak <michal@dudak.me>
2025-01-28 10:56:47 +0100

    Add jest tests

---
+++ jest-tests/test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import { Menu } from '@base-ui-components/react/menu';
+
+test('Menu renders', async () => {
+  const handleItemClick = jest.fn();
+
+  render(
+    <Menu.Root>
+      <Menu.Trigger>Menu</Menu.Trigger>
+      <Menu.Portal>
+        <Menu.Positioner>
+          <Menu.Popup>
+            <Menu.Item onClick={handleItemClick}>Item 1</Menu.Item>
+            <Menu.Item>Item 2</Menu.Item>
+            <Menu.Item>Item 3</Menu.Item>
+          </Menu.Popup>
+        </Menu.Positioner>
+      </Menu.Portal>
+    </Menu.Root>,
+  );
+
+  const menuButton = screen.getByRole('button');
+
+  fireEvent.click(menuButton);
+  await act(async () => {});
+
+  const item1 = screen.queryByText('Item 1');
+  expect(item1).not.toBeNull();
+
+  fireEvent.click(item1!);
+  expect(handleItemClick.mock.calls.length).toBe(1);
+});
